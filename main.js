d8e112add2ba0a406e4a972a43931a05a5dfc6f8
Kp042 <karmapatelkp20092003@gmail.com>
2022-03-23 23:38:23 +0530

    qqqqq

--- main.js
+++ main.js
@@ -446,7 +446,7 @@ app.get('/getPlaceData', verifyToken, (req, res) => {
                 console.log(err);
                 return res.status(500).send('Internal Server Error');
             }
-
+ 
             //send back place data and 200 okay request
             res.status(200).json(results[0]);
         });
@@ -502,10 +502,6 @@ app.get('/getServices', verifyToken, (req, res) => {
         });
     });
 });
-
-
-
-
 
 //generate invoice 
 app.get('/invoice', verifyToken, (req, res) => {
